22f713c557cab97f3d6c312c776da5e926a8553c
Hudson O'Donnell <hudsonodonnell@Hudsons-Air.attlocal.net>
2025-02-09 14:08:19 -0500

    search icon 6

--- app/layout.js
+++ app/layout.js
@@ -11,43 +11,9 @@ export const metadata = {
   }
 }
 
-export default function RootLayout({ children, session }) {
+export default function RootLayout({ children}) {
   return (
     <html lang="en">
-      <head>
-        <link rel="canonical" href="https://athenscreativeco.com/" />
-        <link rel="icon" href="/favicon.ico" sizes="128x128"></link>
-        <title>Athens Creative Co</title>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: `
-              {
-                "@context": "https://schema.org",
-                "@type": "Article",
-                "headline": "Your Article Title",
-                "image": "https://athenscreativeco.com/favicon.ico",
-                "author": {
-                  "@type": "Person",
-                  "name": "Author Name"
-                },
-                "publisher": {
-                  "@type": "Organization",
-                  "name": "Your Company Name",
-                  "logo": {
-                    "@type": "ImageObject",
-                    "url": "https://athenscreativeco.com/favicon.ico"
-                  }
-                },
-                "datePublished": "2025-02-07T08:00:00+00:00",
-                "dateModified": "2025-02-07T09:00:00+00:00"
-              }
-            `,
-          }}
-        />
-        <link rel="icon" href="http://athenscreativeco.com/favicon.ico"></link>
-      </head>
-
       <body className="bg-[#F5F5F5]">
         {children}
       </body>
